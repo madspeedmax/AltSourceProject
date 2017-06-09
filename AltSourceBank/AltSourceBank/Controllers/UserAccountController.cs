42ceff347c85b695182595bd46562874ab3b6117
madspeedmax <hosokawa13@gmail.com>
2017-06-08 20:07:35 -0500

    initial

---
+++ AltSourceBank/AltSourceBank/Controllers/UserAccountController.cs
@@ -0,0 +1,67 @@
+﻿using AltSourceBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AltSourceBank.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        // GET: UserAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (UserList.Users.Any(b => b.Email == model.Email && b.Password == model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.Email, true);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new UserAccount() { Email = model.Email, Password = model.Password };
+                UserList.Users.Add(user);
+                return RedirectToAction("Index", "Home");
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+    }
+}
